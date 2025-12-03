41f39ecc43bf8faeff6bdcd4efefa2f85d6bbcf9
globuli94 <gallusjonathan@gmail.com>
2025-12-03 21:25:07 +0100

    working vue

--- app/assets/javascript/vue/Player.js
+++ app/assets/javascript/vue/Player.js
@@ -2,6 +2,16 @@ const Player = {
     components: {
         Hand
     },
+    data(){
+        return {
+            lastBet: 0,
+        }
+    },
+    watch: {
+        'player.bet'(newBet, oldBet) {
+            this.lastBet = oldBet; // save the old bet
+        }
+    },
     props: {
         player: {
             type: Object,
@@ -14,19 +24,41 @@ const Player = {
         gameUtils: {
             type: Object,
             required: true
-        }
+        },
     },
     methods: {
 
     },
+    computed: {
+
+    },
     template: `
         <div :class="['player', {'current-player': currentPlayer }]">
-            <p class="player-name">{{ player.name }}</p>
+            
             <div class="hand-info d-flex justify-content-center gap-2">
                 <Hand
                     :hand="player.hand"
                     :gameUtils="gameUtils"
                 />
+            </div>
+            
+            <p class="player-name">{{ player.name }}</p>
+            <!-- Money -->
+            <div class="d-flex align-items-center gap-1">
+                <i class="bi bi-bank"></i>
+                <p class="mb-0">{{ player.money }}</p>
+            </div>
+        
+            <!-- Bet -->
+            <div class="mt-1 d-flex align-items-center gap-1">
+                <i class="bi bi-coin"></i>
+                <p v-if="player.state === 'Playing' || player.state === 'Blackjack'" class="mb-0">{{ player.bet }}</p>
+                <p v-if="player.state === 'WON'" class="mb-0 text-won">
+                  +{{ lastBet }}
+                </p>
+                <p v-if="player.state === 'LOST'" class="mb-0 text-lost">
+                  -{{ lastBet }}
+                </p>
             </div>
         </div>
     `
