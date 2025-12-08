506fdcfe631843ddf7f1b3bb0ee65190d0c243f0
imranjan75 <janimran7599@gmail.com>
2025-12-08 13:48:49 +0100

    remove unnessesary code

--- blackjack-vue/src/router/routes.js
+++ blackjack-vue/src/router/routes.js
@@ -1,8 +1,7 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('src/App.vue'),
   },
 
   // Always leave this as last one,
