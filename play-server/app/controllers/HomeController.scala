2cf0c31e47fc8d2d4ff8800fae342e6d7fe0a497
globuli94 <gallusjonathan@gmail.com>
2026-01-07 17:01:47 +0100

    removed frontend from playserver, now only api

--- play-server/app/controllers/HomeController.scala
+++ play-server/app/controllers/HomeController.scala
@@ -1,12 +1,10 @@
 package controllers
 
 import controller.controllerComponent.ControllerInterface
-
 import javax.inject.*
 import play.api.*
 import play.api.mvc.*
 import play.api.libs.json._
-import view.TUI
 import _root_.util.fileIOComponent.JSON.FileIOJSON
 
 import org.apache.pekko.actor.ActorSystem
@@ -18,41 +16,24 @@ import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration.*
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
+ * This controller handles WebSocket connections and JSON API endpoints
+ * for the Blackjack game backend.
  */
 @Singleton
 class HomeController @Inject()(
-                                val controllerComponents: ControllerComponents,
-                                blackjackController: ControllerInterface
-                              ) (implicit system: ActorSystem) extends BaseController {
+  val controllerComponents: ControllerComponents,
+  blackjackController: ControllerInterface
+) (implicit system: ActorSystem) extends BaseController {
 
   // Create FileIOJSON instance to access its implicit JSON formatters
   private val fileIO = new FileIOJSON()
   import fileIO._  // This imports all the implicit JSON formatters (gameWrites, playerWrites, etc.)
 
   /*
-  private val tui: TUI = TUI(blackjackController)
-  private val fileIO = new FileIOJSON()
-  import fileIO._
-  private val tuiThread = new Thread(() => {
-    var input: String = ""
-    val tui = TUI(blackjackController)  // pass injected controller
-
-    while (input != "exit") {
-      input = scala.io.StdIn.readLine()   // blocks this thread only
-      tui.getInputAndPrintLoop(input)     // handle the input
-    }
-  })
-  tuiThread.setDaemon(true)
-  tuiThread.start()
-  */
-
-  /*
-  sets up websocket endpoint where client sends string and server replies string messages
-  creates new BlackjackActor when client connects to handle client
-  actorRef is the connection to the client -> everything sent via out is sent to the client
-  */
+   * Sets up websocket endpoint where client sends string and server replies string messages
+   * creates new BlackjackActor when client connects to handle client
+   * actorRef is the connection to the client -> everything sent via out is sent to the client
+   */
   def socket: WebSocket = WebSocket.accept[String, String] { request =>
     ActorFlow.actorRef { out =>
       println("Connect received")
@@ -69,15 +50,15 @@ class HomeController @Inject()(
   }
 
   /*
-  this class handles messages for one web socket client
-  parameter out represents the endpoint to the client
-
-  func receive always runs when actor receives message from the client
-    -> currently ignores message from client and returns game json back to client
-
-  func sendJsonToClient
-    -> pushes current game state to client
-  */
+   * This class handles messages for one web socket client
+   * parameter out represents the endpoint to the client
+   *
+   * func receive always runs when actor receives message from the client
+   *   -> currently ignores message from client and returns game json back to client
+   *
+   * func sendJsonToClient
+   *   -> pushes current game state to client
+   */
   private class BlackjackWebSocketActor(out: ActorRef, controller: ControllerInterface) extends Actor with Observer {
     implicit val ec: ExecutionContextExecutor = context.dispatcher
     context.system.scheduler.scheduleWithFixedDelay(30.seconds, 30.seconds, self, "heartbeat")
@@ -107,119 +88,6 @@ class HomeController @Inject()(
     }
   }
 
-  def openClient(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.blackjackClient.apply("Client"))
-  }
-
-  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.blackjack.apply(blackjackController))
-  }
-
-  def serialize(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index.apply(blackjackController.toString, blackjackController.serialize))
-  }
-
-  def initializeGame(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    blackjackController.initializeGame()
-    Redirect(routes.HomeController.toGame())
-  }
-
-  def startGame(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    blackjackController.startGame()
-    Redirect(routes.HomeController.toGame())
-  }
-
-  def addPlayerForm(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.addPlayerForm.apply())
-  }
-
-  def addPlayer(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    val player_name_input = request.body
-      .asFormUrlEncoded
-      .flatMap(_.get("PlayerForm").flatMap(_.headOption))
-      .getOrElse("")
-
-    blackjackController.addPlayer(player_name_input)
-    Redirect(routes.HomeController.toGame())
-  }
-
-  def bet(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    val player_bet_input = request.body
-      .asFormUrlEncoded
-      .flatMap(_.get("BetForm").flatMap(_.headOption))
-      .getOrElse("")
-
-    blackjackController.bet(player_bet_input)
-    Redirect(routes.HomeController.toGame())
-  }
-
-  def hitNextPlayer(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    blackjackController.hitNextPlayer()
-    Ok(views.html.blackjack.apply(blackjackController))
-  }
-
-  def standNextPlayer(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    blackjackController.standNextPlayer()
-    Ok(views.html.blackjack.apply(blackjackController))
-  }
-
-  def doubleDown(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    blackjackController.doubleDown()
-    Ok(views.html.blackjack.apply(blackjackController))
-  }
-
-  def leavePlayer(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    blackjackController.leavePlayer()
-    Ok(views.html.blackjack.apply(blackjackController))
-    Redirect(routes.HomeController.toGame())
-  }
-
-  def command(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    val command_input = request.getQueryString("command").getOrElse("")
-    val parts = command_input.split(" ")
-    val command = if (parts.nonEmpty) parts(0) else ""
-    val command_arg = if (parts.length > 1) parts(1) else ""
-
-    command match {
-      case "add" =>
-        blackjackController.addPlayer(command_arg)
-      case "start" =>
-        blackjackController.startGame()
-      case "continue" =>
-        blackjackController.startGame()
-      case "hit" =>
-        blackjackController.hitNextPlayer()
-      case "stand" =>
-        blackjackController.standNextPlayer()
-      case "bet" =>
-        blackjackController.bet(command_arg)
-      case "leave" =>
-        blackjackController.leavePlayer()
-      case "exit" =>
-        blackjackController.exit()
-      case "serialize" =>
-      case _ =>
-    }
-
-    Ok(views.html.index(blackjackController.toString, blackjackController.serialize))
-  }
-
-  def toIndex: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index(blackjackController.toString, blackjackController.serialize))
-  }
-
-  def toGame: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.blackjack.apply(blackjackController))
-  }
-
-  def toHistory: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.history.apply())
-  }
-  
-  def toRule: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.rule.apply())
-  }
-
   private def gameToJson(): JsValue = {
     Json.toJson(blackjackController.getGame)
   }
@@ -228,7 +96,7 @@ class HomeController @Inject()(
     Ok(gameToJson())
   }
 
-  def addPlayerJson(): Action[AnyContent]  = Action{ implicit request =>
+  def addPlayerJson(): Action[AnyContent] = Action { implicit request =>
     request.body.asFormUrlEncoded.flatMap(_.get("PlayerForm").flatMap(_.headOption)) match {
       case Some(name) if name.trim.nonEmpty =>
         blackjackController.addPlayer(name.trim)
@@ -246,28 +114,28 @@ class HomeController @Inject()(
   }
 
   def betJson(): Action[AnyContent] = Action { implicit request =>
-  request.body.asFormUrlEncoded.flatMap(_.get("BetForm").flatMap(_.headOption)) match {
-    case Some(betStr) =>
-      try {
-        blackjackController.bet(betStr)
-        Ok(Json.obj(
-          "success" -> true,
-          "gameState" -> gameToJson()
-        ))
-      } catch {
-        case _: NumberFormatException =>
-          BadRequest(Json.obj(
-            "success" -> false, 
-            "message" -> "Invalid bet amount"
+    request.body.asFormUrlEncoded.flatMap(_.get("BetForm").flatMap(_.headOption)) match {
+      case Some(betStr) =>
+        try {
+          blackjackController.bet(betStr)
+          Ok(Json.obj(
+            "success" -> true,
+            "gameState" -> gameToJson()
           ))
-      }
-    case _ =>
-      BadRequest(Json.obj(
-        "success" -> false, 
-        "message" -> "Bet amount required"
-      ))
+        } catch {
+          case _: NumberFormatException =>
+            BadRequest(Json.obj(
+              "success" -> false, 
+              "message" -> "Invalid bet amount"
+            ))
+        }
+      case _ =>
+        BadRequest(Json.obj(
+          "success" -> false, 
+          "message" -> "Bet amount required"
+        ))
+    }
   }
-}
 
   def hitJson(): Action[AnyContent] = Action {
     blackjackController.hitNextPlayer()
