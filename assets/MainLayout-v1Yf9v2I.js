3f7795aa7fc7bbc9867b0d7aeac8d4ef822e5477
globuli94 <globuli94@users.noreply.github.com>
2026-01-15 20:16:07 +0000

    deploy: edcb3eac91dfb92120304dc51317c3076a0f2ed4

--- assets/MainLayout-v1Yf9v2I.js
+++
@@ -1,1 +0,0 @@
-import{Q as a,a as r}from"./QLayout-CgahaWG6.js";import{c as n,o as s,w as e,r as _,b as o}from"./index-Dr-2UEoX.js";import"./scroll-DmwkYLAC.js";const f={__name:"MainLayout",setup(c){return(p,u)=>{const t=_("router-view");return s(),n(a,{view:"lHh Lpr lFf"},{default:e(()=>[o(r,null,{default:e(()=>[o(t)]),_:1})]),_:1})}}};export{f as default};
